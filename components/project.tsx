263a566f61f36688532a3418c982385da7cac667
developervicky <developer.vicky6@gmail.com>
2024-01-29 16:53:18 +0530

    feat: project motion

--- components/project.tsx
+++ components/project.tsx
@@ -1,10 +1,20 @@
+"use client";
 import React from "react";
 import SectionHeader from "./sectionHeader";
+import { motion } from "framer-motion";
 
 export default function Project() {
   return (
-    <section className="mt-[5rem] flex flex-col gap-3">
+    <motion.section
+      className="mt-[5rem] flex flex-col gap-3"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 100 }}
+      transition={{
+        type: "tween",
+        delay: 0.19,
+      }}
+    >
       <SectionHeader>Project</SectionHeader>;
-    </section>
+    </motion.section>
   );
 }
