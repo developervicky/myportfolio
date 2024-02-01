e9a0ef4c349cc042e005aa853f0388547c5b31b5
developervicky <developer.vicky6@gmail.com>
2024-02-01 10:35:27 +0530

    fix: animation updated

--- components/about.tsx
+++ components/about.tsx
@@ -10,41 +10,44 @@ export default function About() {
   const { ref } = useSectionInView("About");
 
   return (
-    <motion.section
+    <section
       ref={ref}
       id="about"
       className="mt-[6rem] flex max-w-[58rem] scroll-mt-[6.5rem] flex-col items-center justify-center gap-3 px-8 tracking-wide sm:mt-[18rem] sm:px-14  md:mt-[15rem]"
-      initial={{ y: 100, opacity: 0 }}
-      animate={{ y: 0, opacity: 100 }}
-      transition={{
-        type: "tween",
-        delay: 0.17,
-      }}
     >
       <SectionHeader> About Me </SectionHeader>
-      <p className="mt-3 text-center leading-7 sm:indent-8">
-        Hello, I'm Vignesh Kathiresan. At the age of 20, when I've thought of my
-        career options. I had a conviction that I should have to pick career
-        where people rely on lot. That got me to thinking on how much I've been
-        fascinated by technology and web and how it can play a vital role in
-        everyone's daily life that thought led me to give a chance to explore
-        the <span className="font-semibold">web development</span>.
-      </p>
-      <p className="text-center leading-7 sm:indent-8">
-        For over a year, I have been developing full-stack applications on my
-        own projects. My core stack is{" "}
-        <span className="font-semibold">
-          ReactJS, NodeJS, MongoDB and Figma
-        </span>
-        . I am dedicated to creating user-friendly and visually appealing web
-        experiences. These experiences allowed me to enhance my skills and stay
-        up-to-data with the latest technologies and trends.
-      </p>
-      <p className="text-center leading-7 sm:indent-8">
-        Feel free to explore my portfolio to see through my works. If you would
-        like to connect with me, the contact bar is right down, please don't
-        hesitate to reach out.
-      </p>
-    </motion.section>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 100 }}
+        transition={{
+          type: "tween",
+          delay: 0.17,
+        }}
+      >
+        <p className="mt-3 text-center leading-7 sm:indent-8">
+          Hello, I'm Vignesh Kathiresan. At the age of 20, when I've thought of
+          my career options. I had a conviction that I should have to pick
+          career where people rely on lot. That got me to thinking on how much
+          I've been fascinated by technology and web and how it can play a vital
+          role in everyone's daily life that thought led me to give a chance to
+          explore the <span className="font-semibold">web development</span>.
+        </p>
+        <p className="text-center leading-7 sm:indent-8">
+          For over a year, I have been developing full-stack applications on my
+          own projects. My core stack is{" "}
+          <span className="font-semibold">
+            ReactJS, NodeJS, MongoDB and Figma
+          </span>
+          . I am dedicated to creating user-friendly and visually appealing web
+          experiences. These experiences allowed me to enhance my skills and
+          stay up-to-data with the latest technologies and trends.
+        </p>
+        <p className="text-center leading-7 sm:indent-8">
+          Feel free to explore my portfolio to see through my works. If you
+          would like to connect with me, the contact bar is right down, please
+          don't hesitate to reach out.
+        </p>
+      </motion.div>
+    </section>
   );
 }
--- components/sectionHeader.tsx
+++ components/sectionHeader.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function SectionHeader({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <h1 className="text-xl font-semibold sm:text-3xl">{children}</h1>;
+  return (
+    <motion.h1
+      className="text-xl font-semibold sm:text-3xl"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      {children}
+    </motion.h1>
+  );
 }
