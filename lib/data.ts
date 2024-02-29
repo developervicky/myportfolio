138459620c3f19d9edfffac96592eb8cad706893
developervicky <developer.vicky6@gmail.com>
2024-02-29 16:54:14 +0530

    feat: bookEvent added

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,7 @@ import { LuGraduationCap } from "react-icons/lu";
 import studentSpaceImg from "@/public/studentSpace.jpg";
 import tripRoverImg from "@/public/tripRover.jpg";
 import shareThoughtsImg from "@/public/shareThoughts.jpg";
+import bookEventImg from "@/public/bookEvent.jpg";
 import foodieImg from "@/public/foodie.jpg";
 import reactLogo from "@/public/react-2.svg";
 import nodeLogo from "@/public/nodejs-1.svg";
@@ -120,10 +121,46 @@ export const projectsData = [
     ],
   },
   {
+    title: "bookEvent",
+    description:
+      "A CRUD application for booking events. The user friendly place to book your events at few steps.",
+    tags: [
+      "NextJS",
+      "TypeScript",
+      "NodeJS",
+      "Tailwind",
+      "MongoDB",
+      "Shadcn",
+      "Stripe",
+      "UploadThing",
+    ],
+    imageUrl: bookEventImg,
+    website: "https://event-book.vercel.app/",
+    links: [
+      {
+        name: "website",
+        link: "https://event-book.vercel.app/",
+        icon: React.createElement(FiExternalLink),
+      },
+      {
+        name: "github",
+        link: "https://github.com/developervicky/bookEvent",
+        icon: React.createElement(FaGithub),
+      },
+    ],
+  },
+  {
     title: "shareThoughts",
     description:
       "A CRUD application for sharing your thought without getting back any judgemental notes at this modern world.",
-    tags: ["NextJS", "NextAuth", "MongoDB", "Tailwind", "Framer-Motion"],
+    tags: [
+      "NextJS",
+      "React",
+      "Tailwind",
+      "NextAuth",
+      "MongoDB",
+      "Framer-Motion",
+    ],
     imageUrl: shareThoughtsImg,
     website: "https://sharethoughts.vercel.app/",
     links: [
