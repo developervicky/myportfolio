585a6f1b14624472e16832e46c0a9fb98ad895ad
developervicky <developer.vicky6@gmail.com>
2024-03-28 10:02:55 +0530

    refactor: about

--- components/about.tsx
+++ components/about.tsx
@@ -24,7 +24,7 @@ export default function About() {
           delay: 0.17,
         }}
       >
-        <p className="mt-3 text-center leading-7 sm:indent-8 dark:text-gray-400">
+        <p className="mt-3 text-center leading-7 dark:text-gray-400 sm:indent-8">
           Hello, I'm Vignesh Kathiresan. At the age of 20, when I've thought of
           my career options. I had a conviction that I should have to pick
           career where people rely on lot. That got me to thinking on how much
@@ -32,17 +32,17 @@ export default function About() {
           role in everyone's daily life that thought led me to give a chance to
           explore the <span className="font-semibold">web development</span>.
         </p>
-        <p className="text-center leading-7 sm:indent-8 dark:text-gray-400">
+        <p className="text-center leading-7 dark:text-gray-400 sm:indent-8">
           For over a year, I have been developing full-stack applications on my
           own projects. My core stack is{" "}
           <span className="font-semibold">
-            ReactJS, NodeJS, MongoDB and Figma
+            NextJS, Typescript, ReactJS, NodeJS, MongoDB and Figma
           </span>
           . I am dedicated to creating user-friendly and visually appealing web
           experiences. These experiences allowed me to enhance my skills and
           stay up-to-data with the latest technologies and trends.
         </p>
-        <p className="text-center leading-7 sm:indent-8 dark:text-gray-400">
+        <p className="text-center leading-7 dark:text-gray-400 sm:indent-8">
           Feel free to explore my portfolio to see through my works. If you
           would like to connect with me, the contact bar is right down, please
           don't hesitate to reach out.
